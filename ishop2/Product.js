784749a085a6cd616d2940e42038eccfef5b77a7
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2020-12-30 23:05:13 +0300

    Done ishop2

---
+++ ishop2/Product.js
@@ -0,0 +1,40 @@
+var Product = React.createClass({
+  displayName: 'Product',
+
+  propTypes: {
+    code: React.PropTypes.number,
+    name: React.PropTypes.string,
+    price: React.PropTypes.number,
+    url: React.PropTypes.string,
+    quantity: React.PropTypes.number,
+  },
+
+  getInitialState: function () {
+    return {
+      trClass: undefined,
+    }
+  },
+
+  deleteProduct: function (event) {
+    event.stopPropagation();
+    this.setState({ trClass: 'hidden' });
+  },
+
+  highlightProduct: function () {
+    !this.state.trClass ?
+      this.setState({ trClass: 'selected' }) :
+      this.setState({ trClass: undefined })
+  },
+
+  render: function () {
+    return React.DOM.tr({ className: this.state.trClass, onClick: this.highlightProduct },
+      React.DOM.td(null, this.props.name),
+      React.DOM.td(null, this.props.price),
+      React.DOM.td(null, this.props.url),
+      React.DOM.td(null, this.props.quantity),
+      React.DOM.td(null,
+        React.DOM.input({ type: 'button', value: 'удалить', onClick: this.deleteProduct }),
+      )
+    )
+  }
+});
---
+++ ishop2/Store.js
@@ -0,0 +1,66 @@
+﻿var Store = React.createClass({
+  displayName: 'Store',
+
+  propTypes: {
+    productsArray: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number,
+        name: React.PropTypes.string,
+        price: React.PropTypes.number,
+        url: React.PropTypes.string,
+        quantity: React.PropTypes.number,
+      }).isRequired
+    ).isRequired
+  },
+
+  getDefaultProps: function () {
+    return {
+      productsArray: [{
+        code: 0,
+        name: '',
+        price: 0,
+        url: '',
+        quantity: 0,
+      }]
+    }
+  },
+
+  getInitialState: function () {
+    return {
+      headerNameText: 'Имя',
+      headerPriceText: 'Цена',
+      headerUrlText: 'URL',
+      headerQuantityText: 'Количество',
+      headerControlText: 'Контроль',
+    }
+  },
+
+  render: function () {
+    var tableHeader = [];
+    var productsCode = [];
+
+    for (let i = 0; i < Object.keys(this.state).length; i++) {
+      tableHeader.push(React.DOM.th({ key: i }, Object.values(this.state)[i]));
+    }
+
+    this.props.productsArray.forEach(function (item) {
+      productsCode.push(
+        React.createElement(Product, {
+          key: item.code,
+          code: item.code,
+          name: item.name,
+          price: item.price,
+          url: item.url,
+          quantity: item.quantity,
+        })
+      );
+    })
+
+    return React.DOM.table(null,
+      React.DOM.tbody(null,
+        React.DOM.tr(null, tableHeader),
+        productsCode,
+      )
+    );
+  },
+});
