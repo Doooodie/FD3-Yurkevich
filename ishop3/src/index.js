f17b7e165c66c0e7f1b1995a5f12316b446d8b9c
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2021-01-22 15:51:54 +0300

    Deleted files to create new branch

--- ishop3/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Store from './components/Store.js';
-
-const productsArray = require('./products.json');
-const tableHeader = require('./tableHeader.json');
-
-ReactDOM.render(<Store productsArray={productsArray} tableHeader={tableHeader} />, document.querySelector('#root'));
