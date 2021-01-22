5875d62fdbe7e67f0683f07f660453e5af4a7086
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2021-01-22 15:50:32 +0300

    Added some check

--- ishop3/src/components/ProductDescription.js
+++ ishop3/src/components/ProductDescription.js
@@ -3,33 +3,76 @@ import '../styles/ProductDescription.css';
 
 export default class ProductDescription extends Component {
   state = {
-    code: this.props.code,
-    name: this.props.name,
-    price: this.props.price,
-    url: this.props.url,
-    quantity: this.props.quantity,
-    description: this.props.description,
+    code: '',
+    name: '',
+    price: '',
+    url: '',
+    quantity: '',
+    description: '',
+
+    isButtonDisabled: false,
   };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.code !== prevProps.code) {
+      this.setState({
+        code: this.props.code,
+        name: this.props.name,
+        price: this.props.price,
+        url: this.props.url,
+        quantity: this.props.quantity,
+        description: this.props.description,
+      });
+    }
+  }
 
   checkEmptyInput = event => {
     if (event.target.value == '') {
       event.target.classList.add('red-border');
+      this.setState({ isButtonDisabled: true });
     } else {
       event.target.classList.remove('red-border');
+      this.setState({ isButtonDisabled: false });
     }
-  }
+  };
+
+  checkChangedInput = () => {
+    if (
+      this.props.name !== this.state.name ||
+      this.props.price !== this.state.price ||
+      this.props.url !== this.state.url ||
+      this.props.quantity !== this.state.quantity
+    ) {
+      console.log('Не совпадает!')
+    }
+  };
 
   handleChange = event => {
     this.checkEmptyInput(event);
+    this.checkChangedInput();
+    switch (event.target.dataset.type) {
+      case 'name':
+        this.setState({ name: event.target.value });
+        break;
+      case 'price':
+        this.setState({ price: event.target.value });
+        break;
+      case 'url':
+        this.setState({ url: event.target.value });
+        break;
+      case 'quantity':
+        this.setState({ quantity: event.target.value });
+        break;
+    }
   };
 
   render() {
     if (this.props.mode == 'description') {
       return (
         <div>
-          <h1>{this.props.name}</h1>
-          <p>{this.props.description}</p>
-          <p>Price: {this.props.price}</p>
+          <h1>{this.state.name}</h1>
+          <p>{this.state.description}</p>
+          <p>Price: {this.state.price}</p>
         </div>
       );
     } else if (this.props.mode == 'editor') {
@@ -41,35 +84,35 @@ export default class ProductDescription extends Component {
             </caption>
             <tbody>
               <tr>
-                <td>ID: {this.props.code}</td>
+                <td>ID: {this.state.code}</td>
               </tr>
               <tr>
                 <td>Имя</td>
                 <td>
-                  <input type='text' defaultValue={this.props.name} onChange={this.handleChange} />
+                  <input type='text' data-type='name' defaultValue={this.state.name} onChange={this.handleChange} />
                 </td>
               </tr>
               <tr>
                 <td>Цена</td>
                 <td>
-                  <input type='text' value={this.props.price} onChange={this.handleChange} />
+                  <input type='text' data-type='price' value={this.state.price} onChange={this.handleChange} />
                 </td>
               </tr>
               <tr>
                 <td>URL</td>
                 <td>
-                  <input type='text' value={this.props.url} onChange={this.handleChange} />
+                  <input type='text' data-type='url' value={this.state.url} onChange={this.handleChange} />
                 </td>
               </tr>
               <tr>
                 <td>Количество</td>
                 <td>
-                  <input type='text' value={this.props.quantity} onChange={this.handleChange} />
+                  <input type='text' data-type='quantity' value={this.state.quantity} onChange={this.handleChange} />
                 </td>
               </tr>
             </tbody>
           </table>
-          <input type='button' value='Сохранить' onClick={this.changeModeToCreateNewProduct} />
+          <input type='button' disabled={this.state.isButtonDisabled} value='Сохранить' onClick={this.logText} />
           <input type='button' value='Отмена' onClick={this.changeModeToCreateNewProduct} />
         </>
       );
