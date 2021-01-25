eb97f694c7dfa35a8682ade953b5d5ae89e5b4de
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2021-01-25 20:47:36 +0300

    Avoid returning null

--- ishop3/src/components/ProductDescription.js
+++ ishop3/src/components/ProductDescription.js
@@ -43,7 +43,7 @@ export default class ProductDescription extends Component {
       this.props.url !== this.state.url ||
       this.props.quantity !== this.state.quantity
     ) {
-      console.log('Не совпадает!')
+      console.log('Не совпадает!');
     }
   };
 
@@ -67,98 +67,96 @@ export default class ProductDescription extends Component {
   };
 
   render() {
-    if (this.props.mode === 'description') {
-      return (
-        <div>
-          <h1>{this.state.name}</h1>
-          <p>{this.state.description}</p>
-          <p>Price: {this.state.price}</p>
-        </div>
-      );
-    } else if (this.props.mode === 'editor') {
-      return (
-        <>
-          <table className='editor-table'>
-            <caption>
-              <h2>Изменить существующий товар</h2>
-            </caption>
-            <tbody>
-              <tr>
-                <td>ID: {this.state.code}</td>
-              </tr>
-              <tr>
-                <td>Имя</td>
-                <td>
-                  <input type='text' data-type='name' value={this.state.name} onChange={this.handleChange} />
-                </td>
-              </tr>
-              <tr>
-                <td>Цена</td>
-                <td>
-                  <input type='text' data-type='price' value={this.state.price} onChange={this.handleChange} />
-                </td>
-              </tr>
-              <tr>
-                <td>URL</td>
-                <td>
-                  <input type='text' data-type='url' value={this.state.url} onChange={this.handleChange} />
-                </td>
-              </tr>
-              <tr>
-                <td>Количество</td>
-                <td>
-                  <input type='text' data-type='quantity' value={this.state.quantity} onChange={this.handleChange} />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <input type='button' disabled={this.state.isButtonDisabled} value='Сохранить' onClick={this.logText} />
-          <input type='button' value='Отмена' onClick={this.changeModeToCreateNewProduct} />
-        </>
-      );
-    } else if (this.props.mode === 'newProductEditor') {
-      return (
-        <>
-          <table className='editor-table'>
-            <caption>
-              <h2>Добавить новый товар</h2>
-            </caption>
-            <tbody>
-              <tr>
-                <td>ID: {this.props.code}</td>
-              </tr>
-              <tr>
-                <td>Имя</td>
-                <td>
-                  <input type='text' />
-                </td>
-              </tr>
-              <tr>
-                <td>Цена</td>
-                <td>
-                  <input type='text' />
-                </td>
-              </tr>
-              <tr>
-                <td>URL</td>
-                <td>
-                  <input type='text' />
-                </td>
-              </tr>
-              <tr>
-                <td>Количество</td>
-                <td>
-                  <input type='text' />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <input type='button' value='Сохранить' onClick={this.changeModeToCreateNewProduct} />
-          <input type='button' value='Отмена' onClick={this.changeModeToCreateNewProduct} />
-        </>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <>
+        {this.props.mode === 'description' && (
+          <div>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.description}</p>
+            <p>Price: {this.state.price}</p>
+          </div>
+        )}
+        {this.props.mode === 'editor' && (
+          <>
+            <table className='editor-table'>
+              <caption>
+                <h2>Изменить существующий товар</h2>
+              </caption>
+              <tbody>
+                <tr>
+                  <td>ID: {this.state.code}</td>
+                </tr>
+                <tr>
+                  <td>Имя</td>
+                  <td>
+                    <input type='text' data-type='name' value={this.state.name} onChange={this.handleChange} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Цена</td>
+                  <td>
+                    <input type='text' data-type='price' value={this.state.price} onChange={this.handleChange} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>URL</td>
+                  <td>
+                    <input type='text' data-type='url' value={this.state.url} onChange={this.handleChange} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Количество</td>
+                  <td>
+                    <input type='text' data-type='quantity' value={this.state.quantity} onChange={this.handleChange} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <input type='button' disabled={this.state.isButtonDisabled} value='Сохранить' onClick={this.logText} />
+            <input type='button' value='Отмена' onClick={this.changeModeToCreateNewProduct} />
+          </>
+        )}
+        {this.props.mode === 'newProductEditor' && (
+          <>
+            <table className='editor-table'>
+              <caption>
+                <h2>Добавить новый товар</h2>
+              </caption>
+              <tbody>
+                <tr>
+                  <td>ID: {this.props.code}</td>
+                </tr>
+                <tr>
+                  <td>Имя</td>
+                  <td>
+                    <input type='text' />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Цена</td>
+                  <td>
+                    <input type='text' />
+                  </td>
+                </tr>
+                <tr>
+                  <td>URL</td>
+                  <td>
+                    <input type='text' />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Количество</td>
+                  <td>
+                    <input type='text' />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <input type='button' value='Сохранить' onClick={this.changeModeToCreateNewProduct} />
+            <input type='button' value='Отмена' onClick={this.changeModeToCreateNewProduct} />
+          </>
+        )}
+      </>
+    );
   }
 }
