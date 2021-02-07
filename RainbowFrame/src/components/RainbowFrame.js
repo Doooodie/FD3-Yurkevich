dba10d9f8e723974138c11709c0cfe187f73021b
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2021-02-08 02:50:46 +0300

    Added some more methods to render

--- RainbowFrame/src/components/RainbowFrame.js
+++ RainbowFrame/src/components/RainbowFrame.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 export default class RainbowFrame extends Component {
   render() {
     const { colors, children } = this.props;
+
+    /* reduce
+
     const colorsCode = colors.reduce((prevItem, item, index) => {
       return (
         <div key={index} style={{ border: `solid ${item}` }}>
@@ -11,6 +14,33 @@ export default class RainbowFrame extends Component {
       );
     }, children);
 
-    return <>{colorsCode}</>;
+    */
+
+    /* forEach
+
+    let colorsCode = children;
+    colors.forEach((item, index) => {
+      colorsCode = (
+        <div key={index} style={{ border: `solid ${item}` }}>
+          {colorsCode}
+        </div>
+      );
+    });
+
+    */
+
+    // Recursion
+
+    if (colors.length > 0) {
+      return (
+        <div style={{ border: `solid ${colors[0]}` }}>
+          <RainbowFrame colors={colors.slice(1, colors.length)}>
+            {children}
+          </RainbowFrame>
+        </div>
+      );
+    }
+
+    return <div>{children}</div>;
   }
 }
