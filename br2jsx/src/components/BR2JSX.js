3faf288c420c666fd2446ae1667d9a49a59e899a
Kostya Yurkevich <58595156+Doooodie@users.noreply.github.com>
2021-02-08 02:16:59 +0300

    Fixed regexp

--- br2jsx/src/components/BR2JSX.js
+++ br2jsx/src/components/BR2JSX.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class BR2JSX extends Component {
   render() {
     const { text } = this.props;
-    const wordsArray = text.replace(/(\<br\s*\/?\>\s*)+/g, ' br ').split(' ');
+    const wordsArray = text.replace(/<br\s?\/?>/gi, ' br ').split(' ');
     const wordsCode = wordsArray.map((item, index) => {
       if (item === 'br') return <br key={index}/>;
       return item
